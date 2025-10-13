d02124d3f43de97070895c07c9e0e39e4d90e29a
kklearnsAI <keshavkhalap@yahoo.com>
2025-10-12 20:08:49 -0400

    Created Sprint boot Maven project

---
+++ src/test/java/com/hexaware/banking/BankingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaware.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
