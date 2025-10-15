28126ecdeed38dd3f711e5f095724f90cbec65e1
Sayantani27 <sayantani.mondal27@gmail.com>
2025-10-15 07:41:53 -0400

    Base structure

--- src/main/java/com/hexaware/bankmanagementsystem/controller/PaymentModuleController.java
+++ src/main/java/com/hexaware/bankmanagementsystem/controller/PaymentModuleController.java
@@ -3,20 +3,52 @@ package com.hexaware.bankmanagementsystem.controller;
 import com.hexaware.bankmanagementsystem.entity.Payment;
 import com.hexaware.bankmanagementsystem.service.PaymentModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/payments")
 public class PaymentModuleController {
-@Autowired
-private PaymentModuleService paymentModuleService;
+//@Autowired
+//private PaymentModuleService paymentModuleService;
+//
+//@GetMapping
+//public List<Payment> getPayments() {
+//return paymentModuleService.getAllPayments();
+//}
+private final PaymentModuleService paymentModuleService;
+
+public PaymentModuleController(PaymentModuleService paymentModuleService){
+    this.paymentModuleService = paymentModuleService;
+}
 
 @GetMapping
-public List<Payment> getPayments() {
-return paymentModuleService.getAllPayments();
+    public ResponseEntity<List<Payment>> getAllPayments(){
+return new ResponseEntity<>(paymentModuleService.getAllPayments(), HttpStatus.OK);
 }
+
+//@PostMapping
+//public ResponseEntity<Payment> addPayment(@RequestBody Payment payment){
+//return new ResponseEntity<>(paymentModuleService.addPayment(payment),HttpStatus.OK);
+//    }
+
+@GetMapping("/{index}")
+public ResponseEntity<Payment> getPayment(@PathVariable("index") Integer index){
+    return new ResponseEntity<>(paymentModuleService.getPayment(index),HttpStatus.OK);
+}
+
+@PutMapping("/{index}")
+public ResponseEntity<Payment> updatePaymentById(@PathVariable("index") Integer index,@RequestBody Payment payment){
+    return new ResponseEntity<>(paymentModuleService.updatePaymentById(index,payment),HttpStatus.OK);
+    }
+
+@DeleteMapping("/{index}")
+    public ResponseEntity<Void> deletePaymentById(@PathVariable("index") int index){
+        paymentModuleService.deletePaymentById(index);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
