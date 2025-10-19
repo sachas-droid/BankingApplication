c17f1f05b3e524cc9a68bfca38e2f39b88362012
kklearnsAI <keshavkhalap@yahoo.com>
2025-10-18 20:39:49 -0400

    Fixed:Addressed review comments

--- src/main/java/com/hexaware/bankmangementsystem/BankApplication.java
+++ src/main/java/com/hexaware/bankmangementsystem/BankApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(com.hexaware.bankmanagementsystem.BankApplication.class, args);
+		SpringApplication.run(BankApplication.class, args);
 	}
+
 }
